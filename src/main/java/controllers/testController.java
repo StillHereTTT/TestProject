756a4ca7a213281059ae0227668388df57dc1306
DJ026743 <tiandawei@djbx.com>
2020-07-01 13:38:13 +0800

    小程序提交

---
+++ src/main/java/controllers/testController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by DJ026743 on 2020/6/24.
+ */
+@Controller
+@RequestMapping("/home")
+public class testController {
+
+    @RequestMapping("/index")
+    public String index() {
+        System.out.println("111");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
